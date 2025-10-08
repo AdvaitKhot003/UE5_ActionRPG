86e0503ed58f02a26ca025e28e637f9681cd5ec6
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-10-08 15:34:14 +0530

    Native Gameplay Tags

--- Source/Warrior/Private/GameplayTags/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/GameplayTags/WarriorGameplayTags.cpp
@@ -3,3 +3,9 @@
 
 #include "GameplayTags/WarriorGameplayTags.h"
 
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(Input_Move, "Input.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Look, "Input.Look");
+}
--- Source/Warrior/Public/GameplayTags/WarriorGameplayTags.h
+++ Source/Warrior/Public/GameplayTags/WarriorGameplayTags.h
@@ -1,3 +1,12 @@
 // Copyright Advait Khot
 
 #pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Move);
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Look);
+}
