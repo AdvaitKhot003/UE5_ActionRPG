0bf7cf17cf71b636cc15388e8741a7b764f83a1f
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-10-08 14:26:54 +0530

    Debug Helper

---
+++ Source/Warrior/Public/DebugHelpers/WarriorDebugHelper.h
@@ -0,0 +1,16 @@
+﻿// Copyright Advait Khot
+
+#pragma once
+
+namespace Debug
+{
+	static void Print(const FString& Message, const FColor& Color = FColor::MakeRandomColor(), const int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(InKey, 10.f, Color, Message);
+
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+		}
+	}
+}
