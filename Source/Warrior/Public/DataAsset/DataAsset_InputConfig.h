add5dc54f1b8d8c4d23811fdb189074b7bdabad7
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-10-16 22:19:14 +0530

    Input Config Data Asset

---
+++ Source/Warrior/Public/DataAsset/DataAsset_InputConfig.h
@@ -0,0 +1,41 @@
+// No Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FWarriorInputActionConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction = nullptr;
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* InputMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorInputActionConfig> NativeInputActions;
+
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag) const;
+};
