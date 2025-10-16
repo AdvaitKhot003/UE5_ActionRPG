dfe2552d3937d3f4460648c2828fa882c2894c8c
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-10-16 23:39:28 +0530

    Custom Enhanced Input Component

---
+++ Source/Warrior/Private/Components/Input/WarriorInputComponent.cpp
@@ -0,0 +1,5 @@
+// No Copyright
+
+
+#include "Components/Input/WarriorInputComponent.h"
+
---
+++ Source/Warrior/Public/Components/Input/WarriorInputComponent.h
@@ -0,0 +1,34 @@
+// No Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAsset/DataAsset_InputConfig.h"
+#include "WarriorInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	template<class UserObject, typename CallbackFunction>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag,
+		ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunction Function);
+};
+
+template <class UserObject, typename CallbackFunction>
+void UWarriorInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig,
+	const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunction Function)
+{
+	checkf(InInputConfig, TEXT("Warning: UDataAsset_InputConfig is nullptr."));
+
+	if (UInputAction* FoundInputAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundInputAction, TriggerEvent, ContextObject, Function);
+	}
+}
