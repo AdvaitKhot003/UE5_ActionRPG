69f75f09e4ed7ad3c7a2853a65c0e945dc5e2545
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-12-28 00:47:04 +0530

    Calculate Final Damage Done

--- Source/Warrior/Private/Components/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/Components/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -5,10 +5,12 @@
 #include "Components/AbilitySystem/WarriorAttributeSet.h"
 #include "WarriorGameplayTags.h"
 
+#include "WarriorDebugHelper.h"
+
 struct FWarriorDamageCapture
 {
-	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
-	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower);
 
 	FWarriorDamageCapture()
 	{
@@ -41,20 +43,46 @@ void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustom
 	float SourceAttackPower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
 		GetWarriorDamageCapture().AttackPowerDef, EvaluateParameters, SourceAttackPower);
+	Debug::Print(TEXT("SourceAttackPower"), SourceAttackPower);
 
-	const float BaseDamage =
-		OwningGESpec.GetSetByCallerMagnitude(
-			WarriorGameplayTags::SharedTag_SetByCaller_BaseDamage, false, 0.f);
+	float BaseDamage = OwningGESpec.GetSetByCallerMagnitude(
+		WarriorGameplayTags::SharedTag_SetByCaller_BaseDamage, false, 0.f);
+	Debug::Print(TEXT("BaseDamage"), BaseDamage);
 
-	const float CachedLightAttackComboCount =
-		OwningGESpec.GetSetByCallerMagnitude(
-			WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Light, false, 0.f);
+	const float CachedLightAttackComboCount = OwningGESpec.GetSetByCallerMagnitude(
+		WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Light, false, 0.f);
+	Debug::Print(TEXT("CachedLightAttackComboCount"), CachedLightAttackComboCount);
 
-	const float CachedHeavyAttackComboCount =
-		OwningGESpec.GetSetByCallerMagnitude(
-			WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Heavy, false, 0.f);
+	const float CachedHeavyAttackComboCount = OwningGESpec.GetSetByCallerMagnitude(
+		WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Heavy, false, 0.f);
+	Debug::Print(TEXT("CachedHeavyAttackComboCount"), CachedHeavyAttackComboCount);
 	
 	float TargetDefensePower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
 		GetWarriorDamageCapture().DefensePowerDef, EvaluateParameters, TargetDefensePower);
+	Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
+
+	if (CachedHeavyAttackComboCount > 0.f)
+	{
+		const float HeavyAttackComboMultiplier = 1.5f + (CachedHeavyAttackComboCount - 1.f) * 0.5f;
+
+		BaseDamage *= HeavyAttackComboMultiplier;
+		Debug::Print(TEXT("BaseDamageHeavyAttack"), BaseDamage);
+	}
+	else if (CachedLightAttackComboCount > 0.f)
+	{
+		const float LightAttackComboMultiplier = 1.0f + (CachedLightAttackComboCount - 1.f) * 0.25f;
+
+		BaseDamage *= LightAttackComboMultiplier;
+		Debug::Print(TEXT("BaseDamageLightAttack"), BaseDamage);
+	}
+
+	const float FinalDamageDone = BaseDamage * (SourceAttackPower / (SourceAttackPower + TargetDefensePower));
+	Debug::Print(TEXT("FinalDamageDone"), FinalDamageDone);
+
+	if (FinalDamageDone > 0.f)
+	{
+		OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(
+			UWarriorAttributeSet::GetDamageTakenAttribute(), EGameplayModOp::Override, FinalDamageDone));
+	}
 }
--- Source/Warrior/Public/WarriorDebugHelper.h
+++ Source/Warrior/Public/WarriorDebugHelper.h
@@ -13,4 +13,16 @@ namespace Debug
 			UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 		}
 	}
+
+	static void Print(const FString& FloatTitle, float FloatValueToPrint, const FColor& Color = FColor::MakeRandomColor(), int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			const FString Message = FString::Printf(TEXT("%s: %f"), *FloatTitle, FloatValueToPrint);
+
+			GEngine->AddOnScreenDebugMessage(InKey, 10.f, Color, Message);
+
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+		}
+	}
 }
