9e2f04e0548d85c94041e155c7cca028d01c7a68
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-12-27 18:37:49 +0530

    Retrieve Hero Damage Info

--- Source/Warrior/Private/Components/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/Components/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -3,6 +3,7 @@
 
 #include "Components/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
 #include "Components/AbilitySystem/WarriorAttributeSet.h"
+#include "WarriorGameplayTags.h"
 
 struct FWarriorDamageCapture
 {
@@ -26,4 +27,34 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 {
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
+
+void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const FGameplayEffectSpec& OwningGESpec = ExecutionParams.GetOwningSpec();
+	
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = OwningGESpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParameters.TargetTags = OwningGESpec.CapturedTargetTags.GetAggregatedTags();
+
+	float SourceAttackPower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
+		GetWarriorDamageCapture().AttackPowerDef, EvaluateParameters, SourceAttackPower);
+
+	const float BaseDamage =
+		OwningGESpec.GetSetByCallerMagnitude(
+			WarriorGameplayTags::SharedTag_SetByCaller_BaseDamage, false, 0.f);
+
+	const float CachedLightAttackComboCount =
+		OwningGESpec.GetSetByCallerMagnitude(
+			WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Light, false, 0.f);
+
+	const float CachedHeavyAttackComboCount =
+		OwningGESpec.GetSetByCallerMagnitude(
+			WarriorGameplayTags::PlayerTag_SetByCaller_AttackType_Heavy, false, 0.f);
+	
+	float TargetDefensePower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
+		GetWarriorDamageCapture().DefensePowerDef, EvaluateParameters, TargetDefensePower);
 }
